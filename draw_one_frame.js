58a7d1cae155fd90564a4cdc8362c714a8f2b24e
jwm197 <jacob.mcbride@hotmail.co.nz>
2023-03-02 15:38:22 +1300

    Added a jump animation and fixed the launch file to launch the index file

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,19 +1,29 @@
-let characterY=height/2;
+let characterY=0+canvasHeight/2;
 
 function draw_one_frame(cur_frac) {
 	fill(255,255,255);
 	
 	rectMode(CORNER);
-	//rect(0,0,width,height);
-	drawCharacter();
+	rect(0,0,width,height);
+	drawCharacter(cur_frac);
 	drawGround();
 }
-function drawCharacter(){
-	stroke(0,0,0);
-	fill(255,0,255);
-	strokeWeight(1);
+function drawCharacter(cur_frac){
 	rectMode(CENTER);
-	rect(width/10,characterY,width/20,height/5);
+	//stroke(0,0,0);
+	fill(255,0,255);
+	strokeWeight(width/500);
+	if(cur_frac>=.1&&cur_frac<=.5){
+		characterY=map(cur_frac, 0.1, .5, canvasHeight/2, canvasHeight/4);
+	}
+	else if(cur_frac>=.5&&cur_frac<=.9){
+		characterY=map(cur_frac, 0.5, .9,canvasHeight/4, canvasHeight/2);
+	}
+	
+	console.log(characterY);
+	rect(0+2*width/10,characterY,width/20,height/5);
+	
+	
 }
 function drawGround(){
 
