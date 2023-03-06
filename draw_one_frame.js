1e45378046d360d9b5c8fb91b192e990400a6c32
jwm197 <jacob.mcbride@hotmail.co.nz>
2023-03-07 11:21:27 +1300

    Reblocked the scene

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,6 +1,6 @@
 
-const jumpStart=.6;
-const jumpEnd=1;
+const jumpStart=.4;
+const jumpEnd=.9;
 const jumpTop=(jumpStart+jumpEnd)/2;
 const jumpHeight=canvasHeight/4;
 const groundY=canvasHeight/2;
@@ -9,20 +9,33 @@ function draw_one_frame(cur_frac) {
 	fill(255,255,255);
 	rectMode(CORNER);
 	rect(0,0,width,height);
+	//rectMode(CENTER);
+	
+	push();
+	translate(0, canvasHeight/6);
+	//fill(0,0,0);
 	drawCharacter(cur_frac);
- 	drawGround(cur_frac);
+	drawGround(cur_frac);
 	drawTree(cur_frac);
-	drawScore(cur_frac);
+	//drawScore(cur_frac);
+	pop();
+	
 }
+function drawCloud(){
+
+}
+
 function drawScore(cur_frac){
+	fill(0,0,0);
+	strokeWeight(width/500);
 	let score=floor(map(cur_frac,0,1,0,99));
 	textSize(20);
-	text('SCORE: '+score, 0+6/8*width, canvasHeight/10);
+	text('SCORE: '+score, 0+6/8*width, 0+height/10);
 }
 function drawCharacter(cur_frac){
 	rectMode(CENTER);
 	//stroke(0,0,0);
-	fill(255,0,255);
+	//fill(255,0,255);
 	strokeWeight(width/500);
 	if(cur_frac>=jumpStart&&cur_frac<=jumpTop){
 		characterY=map(cur_frac, jumpStart, jumpTop, groundY-canvasHeight/10, jumpHeight);
@@ -35,9 +48,9 @@ function drawCharacter(cur_frac){
 	}
 	
 	//console.log(characterY);
-	console.log(cur_frac);
+	//console.log(cur_frac);
 	//console.log(groundY-height/10);
-	rect(0+2*width/10,characterY,width/20,canvasHeight/5);
+	rect(width/5,characterY,width/20,canvasHeight/5);
 	
 	
 }
@@ -45,8 +58,21 @@ function drawGround(cur_frac){
 	line(0, groundY, width, groundY);
 }
 function drawTree(cur_frac){
-	let treeX=map(cur_frac,0,1,canvasWidth+canvasWidth/20,0-canvasWidth/20);
+	// let treeX=map(cur_frac,0,1,canvasWidth+canvasWidth/20,0-canvasWidth/20);
+	// rectMode(CENTER);
+	// rect(treeX,groundY-canvasHeight/20,canvasWidth/20,canvasHeight/10);
+
+	
+	
+
+
+	let treeX=map(cur_frac,0,1,canvasWidth/2,0);
+	//line()
 	rectMode(CENTER);
 	rect(treeX,groundY-canvasHeight/20,canvasWidth/20,canvasHeight/10);
-	if(cur_frac>)
+	//push();
+	//translate(width/2,0);
+	rect(treeX+width/2,groundY-canvasHeight/20,canvasWidth/20,canvasHeight/10);
+	//pop();
+
 }
